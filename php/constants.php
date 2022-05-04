36624259b9365f7219d5ad166bb4614a70c5d287
nano <35236525+0nano@users.noreply.github.com>
2022-05-04 14:11:32 +0200

    base of php

---
+++ php/constants.php
@@ -0,0 +1,7 @@
+<?php
+    const DB_SERVER = '127.0.0.1';
+    const DB_PORT = '5432';
+    const DB_NAME = 'solde';
+    const DB_USER = 'postgres';
+    const DB_PASSWORD = 'new_password';
+?>
---
+++ php/database.php
@@ -0,0 +1,13 @@
+<?php
+    include 'constants.php';
+
+    function dbConnect(){
+        $dsn = 'pgsql:dbname='.DB_NAME.';host='.DB_SERVER.';port='.DB_PORT;
+        try{
+            $conn = new PDO($dsn, DB_USER, DB_PASSWORD);
+        } catch (PDOException $e){
+            echo 'Connexion échouée : ' . $e->getMessage();
+        }
+        return $conn;
+    }
+?>
