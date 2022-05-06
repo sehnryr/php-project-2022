93369f59a62cee9d6b785b65943f3a5f7ba1b3a3
Mael-GN <mael.grelneau@gmail.com>
2022-05-06 16:30:44 +0200

    comment

--- php/database.php
+++ php/database.php
@@ -1,23 +1,59 @@
 <?php
+    /**
+     * Main file with main function
+     * @copyright TheBoysCompany
+     * 
+     */
+
+
+    /**
+     * This line will include the file "constants.php" containing all 
+     * the differents variables to connect to the data base like 
+     * user-name, password, ip of the server, ...
+     */
     include 'constants.php';
 
+
+    /**
+     * This function is used to open a session from the database and return 
+     * the protocol that other function need to read, write in the database  
+     */
     function dbConnect(){
         $dsn = 'pgsql:dbname='.DB_NAME.';host='.DB_SERVER.';port='.DB_PORT;
+        /*
+        * DB_NAME, DB_SERVER, DB_PORT are variables contained in constants.php
+        */
         try{
             $conn = new PDO($dsn, DB_USER, DB_PASSWORD);
+            /**
+             * try if it can connect to the database
+             */
         } catch (PDOException $e){
             echo 'Connexion échouée : ' . $e->getMessage();
+            /**
+             * if the connection is not granted or they're is an error, it print an error message
+             */
         }
         return $conn;
     }
+    
 
+    /**
+     * 
+     */
     function verification($db, $id){
         $nid = strtolower($id);
-        $request = 'SELECT email from users u where lower(u.email) = :identifiant';
+        $request = 'SELECT identifiant from compte c where lower(c.identifiant) = :identifiant';
+        /**
+         * the lower(...) is used for transform all the uppercase in the identifiant in lowercase
+         */
         $statement = $db->prepare($request);
         $statement->bindParam(':identifiant', $nid);
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        /**
+         * 
+         */
         if(empty($result)){
             return false;
         }else{
@@ -25,6 +61,10 @@
         }
     }
 
+
+    /**
+     * 
+     */
     function userConnection($db, $email, $password){
         
         $request = 'SELECT id from users u where  u.email = :email and u.password = :passwd';
