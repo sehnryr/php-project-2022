68bee48b9b4ea4d2db76fa443b4dfec1585e7ade
Youn Mélois <youn@melois.dev>
2022-05-17 12:31:37 +0200

    refactor

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     if(!isset($_COOKIE['docto']['email'])){
-        setcookie("docto['email']", "email", time()+5*60);
+        setcookie("docto", "email", time()+5*60);
     }
 
     include 'DoctoLibertain.html';
