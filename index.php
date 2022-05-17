2da528f5986afd0b8e9f53d3d8b4ad3cf551580f
0nano <35236525+0nano@users.noreply.github.com>
2022-05-17 12:31:05 +0200

    change index.php

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
     if(!isset($_COOKIE['docto']['email'])){
         setcookie("docto['email']", "email", time()+5*60);
     }
+
     include 'DoctoLibertain.html';
-    
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL,"/DoctoLibertain.html");
-
-    $server_output = curl_exec($ch);
-
-    curl_close ($ch);
-
-    echo $server_output;
 ?>
