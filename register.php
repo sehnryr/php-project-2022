4df82a703878d00916403ec0d15814438f0ac0ad
Youn Mélois <youn@melois.dev>
2022-05-19 11:00:04 +0200

    fix: name, id

--- register.php
+++ register.php
@@ -6,11 +6,11 @@ require_once LIBRARY_PATH . '/common.php';
 $db = new Database();
 
 if (isset($_POST['register'])) {
-	$firstname = $_POST['firstnameLogin'];
-	$lastname = $_POST['lastnameLogin'];
-	$email = $_POST['emailLogin'];
-	$phoneNumber = $_POST['phoneLogin'];
-	$password = $_POST['passwordLogin'];
+	$firstname = $_POST['firstnameRegister'];
+	$lastname = $_POST['lastnameRegister'];
+	$email = $_POST['emailRegister'];
+	$phoneNumber = $_POST['phoneRegister'];
+	$password = $_POST['passwordRegister'];
 
 
 	try {
@@ -26,15 +26,15 @@ if (isset($_POST['register'])) {
 ?>
 
 <form method="post">
-	<label for="firstnameLogin">First Name</label>
-	<input type="text" id="firstnameLogin" name="firstnameLogin" pattern=".{1,64}" required>
-	<label for="lastnameLogin">Last Name</label>
-	<input type="text" id="lastnameLogin" name="lastnameLogin" pattern=".{1,64}" required>
-	<label for="emailLogin">Email</label>
-	<input type="text" id="emailLogin" name="emailLogin" pattern=".{5,64}" required>
-	<label for="phoneLogin">Phone</label>
-	<input type="text" id="phoneLogin" name="phoneLogin" pattern=".{1,10}" required>
-	<label for="passwordLogin">Password</label>
-	<input type="password" id="passwordLogin" name="passwordLogin" pattern=".+" required>
+	<label for="firstnameRegister">First Name</label>
+	<input type="text" id="firstnameRegister" name="firstnameRegister" pattern=".{1,64}" required>
+	<label for="lastnameRegister">Last Name</label>
+	<input type="text" id="lastnameRegister" name="lastnameRegister" pattern=".{1,64}" required>
+	<label for="emailRegister">Email</label>
+	<input type="text" id="emailRegister" name="emailRegister" pattern=".{5,64}" required>
+	<label for="phoneRegister">Phone</label>
+	<input type="text" id="phoneRegister" name="phoneRegister" pattern=".{1,10}" required>
+	<label for="passwordRegister">Password</label>
+	<input type="password" id="passwordRegister" name="passwordRegister" pattern=".+" required>
 	<button type="submit" name="register">Register</button>
 </form>
