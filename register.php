5b62bbf45be17e38c9df0c686a6b3051b80e02b0
sehnryr <youn@melois.dev>
2022-05-24 00:13:42 +0200

    style

--- register.php
+++ register.php
@@ -15,7 +15,7 @@ if (array_key_exists('register', $_POST)) {
     $phoneNumber = $_POST['phoneRegister'];
     $password = $_POST['passwordRegister'];
 
-    if($email == $emailConf){
+    if ($email == $emailConf) {
         try {
             $db->createUser($firstname, $lastname, $email, $phoneNumber, $password);
             $db->connectUser($email, $password);
@@ -23,7 +23,7 @@ if (array_key_exists('register', $_POST)) {
         } catch (AuthenticationException $_) {
         } catch (DuplicateEmailException $_) {
         }
-    }else{
+    } else {
         $error_email = true;
     }
 }
@@ -41,70 +41,66 @@ if (array_key_exists('connection', $_POST)) {
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
     <title>Doctolibertain</title>
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fuggles&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Fuggles&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="public_html/css/index.css" />
+    <link rel="stylesheet" href="public_html/css/floating-square-animation.css" />
 </head>
 
-<body style="background-color: #EAF7FD">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<body class="d-flex flex-column">
+    <div class="area">
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <img class="home-image" src="public_html/img/TaeAugust07.svg" />
+    </div>
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <form method="post">
-                <button class="navbar-brand bg-transparent border-0" name="homepage" id="homepage" style="font-family: 'Fuggles', cursive;font-size: 45px;">
-                    DoctoLibertain
+            <a class="navbar-brand" href="index.php">DoctoLibertain</a>
+            <div class="d-flex justify-content-end align-items-center">
+                <button class="btn btn-light" id="btn-pro" disabled>
+                    Vous êtes un professionnel de santé ?
                 </button>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </form>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <div style="font-size: 15px;" class="badge rounded-pill bg-light text-dark">
-                                Vous êtes un professionnel de santé ?
-                            </div>
-                        </a>
-                    </li>
-                    <div class="nav-item">
-                        <img width="16" height="16" src="public_html/img/person_FILL1_wght400_GRAD0_opsz48.svg" alt="Person">
+                <a class="btn btn-outline-light ms-2" href="login.php">
+                    <div class="d-flex align-items-center">
+                        <img class="me-2" style="height: 2rem;" src="public_html/img/person_FILL1_wght400_GRAD0_opsz48.svg">
+                        <div class="d-flex flex-column align-items-start">
+                            <span>Se connecter</span>
+                            <small>Gérer mes rdv</small>
+                        </div>
                     </div>
-                    <li class="nav-item">
-                        <form method="post">
-                            <button class="nav-link container-fluid bg-transparent border-0" name="connection">
-                                <div class="">
-                                    <div class="fw-bolder link-light">
-                                        Se connecter
-                                    </div>
-                                    <div style="opacity: 0.7; font-size: 12px;">
-                                        Gérer mes rdv
-                                    </div>
-                                </div>
-                            </button>
-                        </form>
-                    </li>
-                </ul>
+                </a>
             </div>
         </div>
     </nav>
     <?php
-        if($error_email){
-            echo "<div class=\"alert alert-danger\" role=\"alert\">";
-            echo "L'email et l'email de confirmation sont différentes !!";
-            echo "</div>";
-        }
+    if ($error_email) {
+        echo "<div class=\"alert alert-danger\" role=\"alert\">";
+        echo "L'email et l'email de confirmation sont différentes !!";
+        echo "</div>";
+    }
     ?>
     <div class="container-fluid d-flex justify-content-center">
         <div class="card my-4" style="width: 32rem;">
             <div class="card-body">
-                <p>Nouveau sur DoctoLibertain ?</p>
-                <p>Saisissez vos informations pour continuer</p>
+                <h5 class="card-title">Nouveau sur DoctoLibertain ?</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Saisissez vos informations pour continuer</h6>
                 <form method="POST">
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col">
                             <input type="text" class="form-control" placeholder="Prenom" id="firstnameRegister" name="firstnameRegister" pattern=".{1,64}" required>
                         </div>
@@ -112,31 +108,31 @@ if (array_key_exists('connection', $_POST)) {
                             <input type="text" class="form-control" placeholder="Nom" id="lastnameRegister" name="lastnameRegister" pattern=".{1,64}" required>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col">
                             <input type="text" class="form-control" placeholder="Telephone portable (sinon fixe)" id="phoneRegister" name="phoneRegister" pattern=".{1,10}" required>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col">
                             <input type="text" class="form-control" placeholder="Votre adresse mail" id="emailRegister" name="emailRegister" pattern=".{5,64}" required>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col">
                             <input type="text" class="form-control" placeholder="Confirmez votre adresse mail" id="emailRegisterConf" name="emailRegisterConf" pattern=".{5,64}" required>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col">
                             <input type="password" class="form-control" placeholder="Choisissez un mot de passe" id="passwordRegister" name="passwordRegister" pattern=".{8,256}" required>
                             <small id="passwordHelpInline" class="text-muted">
-                                Must be 8-256 characters long.
+                                Doit contenir entre 8 et 256 caractères.
                             </small>
                         </div>
                     </div>
 
-                    <button type="submit" class="btn btn-primary" name="register">S'INSCRIRE</button>
+                    <button type="submit" class="btn btn-primary" name="register">M'inscrire</button>
 
                 </form>
             </div>
