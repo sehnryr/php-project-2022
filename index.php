f1a14bcc79049988693e6632042085c4a50dde89
Youn Mélois <youn@melois.dev>
2022-05-23 15:46:03 +0200

    remove debugging

--- index.php
+++ index.php
@@ -1,129 +1,128 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-    
 require_once 'resources/config.php';
 require_once 'resources/database.php';
 require_once LIBRARY_PATH . '/common.php';
 require_once LIBRARY_PATH . '/exceptions.php';
 
-    if(array_key_exists('homepage', $_POST)){
-        redirect('');
-    }
-    
-    if(array_key_exists('connection', $_POST)){
-        redirect('login.php');
-    }
-    
-    $db = new Database();
+if (array_key_exists('homepage', $_POST)) {
+  redirect('');
+}
+
+if (array_key_exists('connection', $_POST)) {
+  redirect('login.php');
+}
+
+$db = new Database();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Doctolibertain</title>
-    <link rel="stylesheet" href="public_html/css/index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fuggles&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <title>Doctolibertain</title>
+  <link rel="stylesheet" href="public_html/css/index.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fuggles&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg" style="background-color: #107ACA;">
-        <div class="container-fluid">
-        <form method="post">
-    	    <button class="navbar-brand bg-transparent border-0 text-white" name="homepage" id="homepage" style="font-family: 'Fuggles', cursive;font-size: 45px;">
-			    DoctoLibertain
-		    </button>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-		    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      		    <span class="navbar-toggler-icon"></span>
-    	    </button>
-	    </form>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item mt-2">
-                <a class="nav-link" href="#">
-                    <div style="font-size: 15px;color: #107ACA;" class="badge rounded-pill bg-light">
-                        Vous êtes un professionnel de santé ?
+  <nav class="navbar navbar-expand-lg" style="background-color: #107ACA;">
+    <div class="container-fluid">
+      <form method="post">
+        <button class="navbar-brand bg-transparent border-0 text-white" name="homepage" id="homepage" style="font-family: 'Fuggles', cursive;font-size: 45px;">
+          DoctoLibertain
+        </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </form>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item mt-2">
+            <a class="nav-link" href="#">
+              <div style="font-size: 15px;color: #107ACA;" class="badge rounded-pill bg-light">
+                Vous êtes un professionnel de santé ?
+              </div>
+            </a>
+          </li>
+          <li class="nav-item">
+            <form method="post">
+              <button class="nav-link container-fluid bg-transparent border-0 text-white" name="connection">
+                <ul class="list-inline">
+                  <li class="list-inline-item mb-2">
+                    <img src="public_html/img/person_FILL1_wght400_GRAD0_opsz48.svg" alt="Person" style="transform: translate(0, -1.25vh);">
+                  </li>
+                  <li class="list-inline-item">
+                    <div class="fw-bolder link-light">
+                      Se connecter
                     </div>
-                </a>
-              </li>
-              <li class="nav-item">
-                <form method="post">
-                    <button class="nav-link container-fluid bg-transparent border-0 text-white" name="connection">
-                      <ul class="list-inline">
-                        <li class="list-inline-item mb-2">
-                          <img src="public_html/img/person_FILL1_wght400_GRAD0_opsz48.svg" alt="Person" style="transform: translate(0, -1.25vh);">
-                        </li>
-                        <li class="list-inline-item">
-                          <div class="fw-bolder link-light">
-                            Se connecter
-                          </div>
-                          <div style="opacity: 0.7; font-size: 12px;">
-                            Gérer mes rdv
-                          </div>
-                        </li>
-                      </ul>
-                    </button>
-                </form>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!--barre de recherche + image-->
-      <div class="d-flex align-items-center justify-content-center position-relative" style="height: 20rem; background-image: linear-gradient(#107ACA, #10A7DA); display: grid; grid-template-rows: auto auto auto; grid-template-columns: auto auto auto;"> 
-        <form action="search.php" method="get" style="display: flex; align-items: center; justify-content: center;">
-          <select name="spe" class="form-control" required>
-            <option value="">Quelle spécialité ?</option>
-            <?php
-              $spes = $db->getAllSpecialties();
-              foreach ($spes as $spe) {
-                echo "<option value=\"" . $spe['id'] . "\">" . $spe['name'] . "</option>";
-              }
-            ?>
-          </select>
-          <input type="text" class="form-control" placeholder="Où" name="ou">
-          <button style="background-color:orange;" type="submit" class="btn btn-primary">Recherche</button>
-        </form>
-        <img class="position-absolute bottom-0 end-0" style="resize: both; width: 20vw; height: 20vh;" src="public_html/img/raoul.png">
+                    <div style="opacity: 0.7; font-size: 12px;">
+                      Gérer mes rdv
+                    </div>
+                  </li>
+                </ul>
+              </button>
+            </form>
+          </li>
+        </ul>
       </div>
-      <!-- Actualités -->
-      <div>
-        <p><h4 class="text-center">Actualités à la une :</h4></p>
-        <div>
-        <div class="card mb-3" style="background-color: #EAF7FD;">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="public_html/img/Didier Raoult Actu.jpg" class="img-fluid rounded-start" alt="Photo Didier Raoult">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Mr Raoult fait encore des ravages...</h5>
-                <p class="card-text">Mr Didier Raoult a fait débat son intervention lors d'un conférence sur le plaisir, ...</p>
-                <p class="card-text"><small class="text-muted">Publié, il y a 3 jours</small></p>
-              </div>
+    </div>
+  </nav>
+  <!--barre de recherche + image-->
+  <div class="d-flex align-items-center justify-content-center position-relative" style="height: 20rem; background-image: linear-gradient(#107ACA, #10A7DA); display: grid; grid-template-rows: auto auto auto; grid-template-columns: auto auto auto;">
+    <form action="search.php" method="get" style="display: flex; align-items: center; justify-content: center;">
+      <select name="spe" class="form-control" required>
+        <option value="">Quelle spécialité ?</option>
+        <?php
+        $spes = $db->getAllSpecialties();
+        foreach ($spes as $spe) {
+          echo "<option value=\"" . $spe['id'] . "\">" . $spe['name'] . "</option>";
+        }
+        ?>
+      </select>
+      <input type="text" class="form-control" placeholder="Où" name="ou">
+      <button style="background-color:orange;" type="submit" class="btn btn-primary">Recherche</button>
+    </form>
+    <img class="position-absolute bottom-0 end-0" style="resize: both; width: 20vw; height: 20vh;" src="public_html/img/raoul.png">
+  </div>
+  <!-- Actualités -->
+  <div>
+    <p>
+    <h4 class="text-center">Actualités à la une :</h4>
+    </p>
+    <div>
+      <div class="card mb-3" style="background-color: #EAF7FD;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="public_html/img/Didier Raoult Actu.jpg" class="img-fluid rounded-start" alt="Photo Didier Raoult">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Mr Raoult fait encore des ravages...</h5>
+              <p class="card-text">Mr Didier Raoult a fait débat son intervention lors d'un conférence sur le plaisir, ...</p>
+              <p class="card-text"><small class="text-muted">Publié, il y a 3 jours</small></p>
             </div>
           </div>
-        </div>
-        <div class="card mb-3">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="..." class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </div>
         </div>
       </div>
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
--- user.php
+++ user.php
@@ -1,18 +1,15 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once 'resources/config.php';
 require_once 'resources/database.php';
 require_once LIBRARY_PATH . '/common.php';
 
 $db = new Database();
 
-if(array_key_exists('homepage', $_POST)){
+if (array_key_exists('homepage', $_POST)) {
 	redirect('');
 }
 
-if(array_key_exists('search', $_POST)){
+if (array_key_exists('search', $_POST)) {
 	redirect('');
 }
 
@@ -22,9 +19,9 @@ if (array_key_exists('disconnect', $_POST)) {
 	redirect('');
 }
 
-$access_token = $_COOKIE['docto_session'];
 
 try {
+	$access_token = $_COOKIE['docto_session'];
 	$infos = $db->getUserInfos($access_token);
 } catch (Exception $_) {
 	redirect('login.php');
@@ -34,143 +31,146 @@ try {
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Doctolibertain</title>
-    <link rel="stylesheet" href="public_html/css/index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fuggles&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<title>Doctolibertain</title>
+	<link rel="stylesheet" href="public_html/css/index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fuggles&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-        	<form method="post">
-    	    	<button class="navbar-brand bg-transparent border-0" name="homepage" id="homepage" style="font-family: 'Fuggles', cursive;font-size: 45px;">
-				    DoctoLibertain
-		    	</button>
-		    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-		    	aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      			    <span class="navbar-toggler-icon"></span>
-    	    	</button>
-	    	</form>
-        	<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          		<div class="nav-item">
-		  			<h4 class="text-white"><?php echo $infos['firstname']; ?> <?php echo $infos['lastname']; ?></h4>
-          		</div>
-          		<div class="nav-item mt-3	">
-            		<form method="post">
-               		 	<button class="nav-link container-fluid bg-transparent border-0" name="disconnect">
-                    		<ul class="list-inline">
-								<li class="list-inline-item"><img src="public_html/img/power_settings_new_FILL0_wght400_GRAD0_opsz48.svg" alt="déconnection"></li>
-								<li class="list-inline-item"><p class="text-white">Déconnexion</p></li>
-							</ul>
-                		</button>
-            		</form>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		<div class="container-fluid">
+			<form method="post">
+				<button class="navbar-brand bg-transparent border-0" name="homepage" id="homepage" style="font-family: 'Fuggles', cursive;font-size: 45px;">
+					DoctoLibertain
+				</button>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</form>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+				<div class="nav-item">
+					<h4 class="text-white"><?php echo $infos['firstname']; ?> <?php echo $infos['lastname']; ?></h4>
 				</div>
-          	</div>
-        </div>
-    </nav>
+				<div class="nav-item mt-3	">
+					<form method="post">
+						<button class="nav-link container-fluid bg-transparent border-0" name="disconnect">
+							<ul class="list-inline">
+								<li class="list-inline-item"><img src="public_html/img/power_settings_new_FILL0_wght400_GRAD0_opsz48.svg" alt="déconnection"></li>
+								<li class="list-inline-item">
+									<p class="text-white">Déconnexion</p>
+								</li>
+							</ul>
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</nav>
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-3 d-flex flex-column align-items-start justify-content-center" style="background-color: #C7D0D9; height: 88.2vh">
 				<h5 class="mt-4 text-decoration-underline">Voir les rendez-vous passés :</h5>
 				<div class="col mt-2">
-				<?php
+					<?php
 					$put_an_appointment = false;
 					$appointements = $db->getAllAppointments($infos['id']);
-					if(!empty($appointements)){
-						foreach($appointements as $appointement){
-							if(strtotime($appointement['date_time']) < time()){
+					if (!empty($appointements)) {
+						foreach ($appointements as $appointement) {
+							if (strtotime($appointement['date_time']) < time()) {
 								$put_an_appointment = true;
 								$date = explode(" ", $appointement['date_time']);
 								$doc = $db->getDoctorName($appointement['doctorid']);
 
 								echo "<div class=\"card mt-2\" style=\"width: 18rem;\">";
-  								echo "<div class=\"card-body\">";
+								echo "<div class=\"card-body\">";
 								echo "<h5 class=\"card-title\"><span class=\"badge rounded-pill text-black\" style=\"background-color: #C4C4C4;\">";
 								echo "<img src=\"public_html/img/calendar_month_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\">";
 								echo date("d M Y", strtotime($date[0]));
 								echo "<img src=\"public_html/img/schedule_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"clock\">";
 								echo $date[1];
 								echo "</span></h5>";
-    							echo "<p class=\"card-text\">";
+								echo "<p class=\"card-text\">";
 								echo "Dr. " . $doc['firstname'] . " " . $doc['lastname'];
 								echo "</p>";
 								echo "<p class=\"card-text\">";
 								echo "Spécialté : " . $db->getDoctorSpecialty($appointement['doctorid']);
 								echo "</p>";
-  								echo "</div>";
+								echo "</div>";
 								echo "</div>";
 							}
 						}
-						if(!$put_an_appointment){
+						if (!$put_an_appointment) {
 							echo "<p>Vous n'avez pas pris d'ancien rendez-vous avec DoctoLibertain</p>";
 						}
-					}else{
+					} else {
 						echo "<p>Vous n'avez pas pris d'ancien rendez-vous avec DoctoLibertain</p>";
 					}
-				?>
+					?>
 				</div>
 			</div>
 			<div class="col d-flex align-items-center justify-content-center" style="background-color: #E9EDF1">
 				<div>
 					<?php
-						$put_an_appointment = false;
-						if(!empty($appointements)){
-							foreach ($appointements as $appointement) {
-								if(strtotime($appointement['date_time']) > time()){
-									$put_an_appointment = true;
-									$date = explode(" ", $appointement['date_time']);
-									$doc = $db->getDoctorName($appointement['doctorid']);
+					$put_an_appointment = false;
+					if (!empty($appointements)) {
+						foreach ($appointements as $appointement) {
+							if (strtotime($appointement['date_time']) > time()) {
+								$put_an_appointment = true;
+								$date = explode(" ", $appointement['date_time']);
+								$doc = $db->getDoctorName($appointement['doctorid']);
 
-									echo "<div class=\"card mt-2\" style=\"width: 18rem;\">";
-  									echo "<div class=\"card-body\">";
-									echo "<h5 class=\"card-title\"><span class=\"badge rounded-pill text-black\" style=\"background-color: #C4C4C4;\">";
-									echo "<img src=\"public_html/img/calendar_month_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\">";
-									echo date("d M Y", strtotime($date[0]));
-									echo "<img src=\"public_html/img/schedule_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"clock\">";
-									echo $date[1];
-									echo "</span></h5>";
-    								echo "<p class=\"card-text\">";
-									echo "Dr. " . $doc['firstname'] . " " . $doc['lastname'];
-									echo "</p>";
-									echo "<p class=\"card-text\">";
-									echo "Spécialté : " . $db->getDoctorSpecialty($appointement['doctorid']);
-									echo "</p>";
-  									echo "</div>";
-									echo "</div>";
-								}
+								echo "<div class=\"card mt-2\" style=\"width: 18rem;\">";
+								echo "<div class=\"card-body\">";
+								echo "<h5 class=\"card-title\"><span class=\"badge rounded-pill text-black\" style=\"background-color: #C4C4C4;\">";
+								echo "<img src=\"public_html/img/calendar_month_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\">";
+								echo date("d M Y", strtotime($date[0]));
+								echo "<img src=\"public_html/img/schedule_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"clock\">";
+								echo $date[1];
+								echo "</span></h5>";
+								echo "<p class=\"card-text\">";
+								echo "Dr. " . $doc['firstname'] . " " . $doc['lastname'];
+								echo "</p>";
+								echo "<p class=\"card-text\">";
+								echo "Spécialté : " . $db->getDoctorSpecialty($appointement['doctorid']);
+								echo "</p>";
+								echo "</div>";
+								echo "</div>";
 							}
-							if(!$put_an_appointment){
-								echo "<ul class=\"list-inline\"><li class=\"list-inline-item\">";
-								echo "<img src=\"public_html/img/calendar_add_on_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\" style=\"transform: rotate(-14.17deg) translate(0, -2vh);;\">";
-								echo "</li><li class=\"list-inline-item\"><p>";
-								echo "Aucun rendez-vous à venir<br>";
-								echo "<form method=\"post\">";
-								echo "<button class=\"navbar-brand bg-transparent border-0 m-0 p-0 text-primary\" name=\"search\" style=\"font-size: 15px\">";
-								echo "Prendre un nouveau rendez-vous";
-								echo "</button>";
-								echo "</form>";
-								echo "</p></li></ul>";
-							}
-						}else{
-							echo "<img src=\"public_html/img/calendar_add_on_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\" style=\"transform: rotate(-14.17deg);\">";
-							echo "<p>";
+						}
+						if (!$put_an_appointment) {
+							echo "<ul class=\"list-inline\"><li class=\"list-inline-item\">";
+							echo "<img src=\"public_html/img/calendar_add_on_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\" style=\"transform: rotate(-14.17deg) translate(0, -2vh);;\">";
+							echo "</li><li class=\"list-inline-item\"><p>";
 							echo "Aucun rendez-vous à venir<br>";
 							echo "<form method=\"post\">";
 							echo "<button class=\"navbar-brand bg-transparent border-0 m-0 p-0 text-primary\" name=\"search\" style=\"font-size: 15px\">";
 							echo "Prendre un nouveau rendez-vous";
 							echo "</button>";
 							echo "</form>";
-							echo "</p>";
+							echo "</p></li></ul>";
 						}
+					} else {
+						echo "<img src=\"public_html/img/calendar_add_on_FILL0_wght400_GRAD0_opsz48.svg\" alt=\"calendar\" style=\"transform: rotate(-14.17deg);\">";
+						echo "<p>";
+						echo "Aucun rendez-vous à venir<br>";
+						echo "<form method=\"post\">";
+						echo "<button class=\"navbar-brand bg-transparent border-0 m-0 p-0 text-primary\" name=\"search\" style=\"font-size: 15px\">";
+						echo "Prendre un nouveau rendez-vous";
+						echo "</button>";
+						echo "</form>";
+						echo "</p>";
+					}
 					?>
 				</div>
 			</div>
 		</div>
 	</div>
 </body>
+
 </html>
