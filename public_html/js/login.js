a5d1539faea34d818c649cd5b653536b78d4aa85
sehnryr <youn@melois.dev>
2022-05-24 21:23:58 +0200

    fix login.js

--- public_html/js/login.js
+++ public_html/js/login.js
@@ -4,8 +4,14 @@ import { createCookie, getCookie } from './utils.js'
 $(() => {
 	let cookie = getCookie('docto_session')
 	if (cookie.length > 0) {
-		let url = window.location.href.replace(/login\.html.*/i, 'user.html')
-		window.location.href = url
+		$.ajax("api.php/user", {
+			method: "GET", headers: {
+				Authorization: 'Bearer ' + cookie
+			}
+		}).done((_) => {
+			let url = window.location.href.replace(/login\.html.*/i, 'user.html')
+			window.location.href = url
+		})
 	}
 })
 
