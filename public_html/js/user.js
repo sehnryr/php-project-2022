c56296c046520292d5b076b45cda59d1af2629b1
sehnryr <youn@melois.dev>
2022-05-24 22:15:05 +0200

    past appointments fix

--- public_html/js/user.js
+++ public_html/js/user.js
@@ -4,12 +4,58 @@ import { getCookie, deleteCookie } from './utils.js'
 $(() => {
 	let cookie = getCookie('docto_session')
 	if (cookie.length > 0) {
+
+		// set user name
 		$.ajax("api.php/user", {
 			method: "GET", headers: {
 				Authorization: 'Bearer ' + cookie
 			}
 		}).done((data) => {
 			$("#username").html(data['firstname'] + ' ' + data['lastname'])
+		})
+
+		// set past appointments
+		$.ajax("api.php/appointments", {
+			method: "GET", headers: {
+				Authorization: 'Bearer ' + cookie
+			}
+		}).done((data) => {
+			let result = []
+			data.forEach((element) => {
+				if (element['user_id'] != null && new Date(element['date_time']) < new Date()) {
+					result.push(element)
+				}
+			})
+
+			if (result.length > 0) {
+				$("#pastAppointments").html('')
+				result.forEach((element) => {
+					let card = `
+					<div class="card mt-2" style="width: auto; margin:1em">
+					<div class="card-body">
+					<h5 class="card-title"><span class="badge rounded-pill text-black" style="background-color: #C4C4C4;">
+					<img src="public_html/img/calendar_month_FILL0_wght400_GRAD0_opsz48.svg" alt="calendar">`
+						+ new Date(element['date_time']).toDateString() +
+						`<img src="public_html/img/schedule_FILL0_wght400_GRAD0_opsz48.svg" alt="clock">`
+						+ element['date_time'].split(' ')[1] +
+						`</span></h5><p class="card-text">Dr. `
+						+ element['firstname'] + ' ' + element['lastname'] +
+						`</p><p class="card-text">Spécialté : `
+						+ element['specialty_name'] + `</p>
+						<form method="GET" action="search.html">
+						<input type="text" name="spe" value="`
+						+ element['specialty_id'] + `" hidden>
+						<input type="text" name="nom" value="`
+						+ element['firstname'] + ' ' + element['lastname'] + `" hidden>
+						<button class=" border-0" style="border-radius:5px; width:10em; background-color: #F6C844;">
+						Reprendre un RDV ?
+						</button>
+						</form>
+						</div></div>`
+
+					$("#pastAppointments").append(card)
+				})
+			}
 		})
 	}
 })
